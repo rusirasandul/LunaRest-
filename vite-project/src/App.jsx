76d5394ad6a17d6d62faa2a4bf3e7c9e30c3b465
NihlaNazim <nihlanazim05@gmail.com>
2025-02-22 14:35:57 +0530

    adjust the app.jsx file and modify the tailwind css file

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,15 +1,25 @@
-import { BrowserRouter as Router } from "react-router-dom"
-import LandingPage from "./pages/LandingPage"
+import { BrowserRouter as Router } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Hero from "./components/Hero";
+import Prediction from "./components/Prediction";
+import Setting from "./components/Setting";
+import Journal from "./components/Journal";
+import LandingPage from "./pages/LandingPage";
+import "./App.css";
 
 function App() {
   return (
     <Router>
-      <div className="min-h-screen">
+      <div className="min-h-screen overflow-x-hidden font-heading">
+        <Navbar />
+        <Hero />
+        <Prediction />
+        <Journal />
+        <Setting />
         <LandingPage />
       </div>
     </Router>
-  )
+  );
 }
 
-export default App
-
+export default App;
