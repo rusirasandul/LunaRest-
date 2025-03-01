04a8de18cb22e11dfa7de75072fffd0f99bc3f28
chalitha-wickramasinghe <chaliya.wx@gmail.com>
2025-03-01 18:48:42 +0530

    according to the questions updated some answers for the questions are implemented as user to choose from as options

--- chalitha-project-with-quiz/src/data.js
+++ chalitha-project-with-quiz/src/data.js
@@ -1,98 +1,108 @@
 export const questions = [
   {
     question: "Well done for getting started! What would you like us to call you?",
-    options: ["Toronto", "Vancouver", "Ottawa", "Montreal"],
-    answer: "Ottawa",
+    options: [],
+
   },
   {
     question: "Select your gender?" ,
-    options: ["Venus", "Mars", "Jupiter", "Saturn"],
-    answer: "Mars",
+    options: ["Male", "Female", "Other"],
+    
   },
   {
     question: "Which year are you born in?" ,
-    options: ["Venus", "Mars", "Jupiter", "Saturn"],
-    answer: "Mars",
+    options: [],
   },
   {
     question: "What year are you currently in at university?",
     options: [
-      "Charles Dickens",
-      "William Shakespeare",
-      "Jane Austen",
-      "Mark Twain",
+      "1st Year",
+      "2nd Year",
+      "3rd Year",
+      "4th Year",
     ],
-    answer: "William Shakespeare",
+
   },
   {
     question: "On average, how many hours do you sleep per night?",
     options: [
-      "Atlantic Ocean",
-      "Indian Ocean",
-      "Arctic Ocean",
-      "Pacific Ocean",
+      "Less than 5",
+      "5-6 hours",
+      "6-7 hours",
+      "7-8 hours",
+      "8-9 hours",
+      "10+ hours",
     ],
-    answer: "Pacific Ocean",
+
   },
   {
     question: "On a typical day, how many hours do you spend studying?",
-    options: ["Ag", "Au", "Fe", "Pb"],
-    answer: "Au",
+    options: [
+      "Less than 1 hour",
+      "1-2 hours",
+      "2-3 hours",
+      "3-4 hours",
+      "4-5 hours",
+      "5+ hours"
+    ],
   },
   {
     question: "On a typical day, how many hours do you spend on screens for non-study activities (e.g., social media, gaming, TV) ?",
-    options: ["Neil Armstrong", "Buzz Aldrin", "Yuri Gagarin", "John Glenn"],
-    answer: "Neil Armstrong",
+    options: [
+      "Less than 1 hour",
+      "1-2 hours",
+      "2-3 hours",
+      "3-4 hours",
+      "4-5 hours",
+      "5+ hours"
+    ],
+    
   },
   {
     question: "How many caffeinated drinks do you have per day(coffee, tea, energy drinks) ?",
-    options: ["China", "Japan", "South Korea", "Thailand"],
-    answer: "Japan",
+    options: [
+      "0 (None)",
+      "1 drink",
+      "2 drinks",
+      "3 drinks",
+      "4 drinks",
+      "4+ drinks",
+    ],
+
   },
   {
     question: "How many minutes of vigorous physical activity do you do per day?",
-    options: ["Gold", "Diamond", "Iron", "Quartz"],
-    answer: "Diamond",
+    options: [
+      "0 minutes",
+      "0-20 minutes",
+      "20-40 minutes",
+      "40-60 minutes",
+      "60-90 minutes",
+      "90+ minutes",
+    ],
   },
   {
     question: "How would you rate your sleep quality on a scale of 1 to 10? (1 = Very poor, 10 = Excellent)",
-    options: ["Gold", "Diamond", "Iron", "Quartz"],
-    answer: "Diamond",
+    options: [],
+    
   },
   {
     question: "On weekdays, what time do you usually go to sleep? (24-hour format, e.g., 23:00 for 11 PM)",
-    options: ["Africa", "Asia", "Europe", "South America"],
-    answer: "Africa",
+    options: [],
+    
   },
   {
     question: "On weekends, what time do you usually go to sleep? (24-hour format, e.g., 00:30 for 12:30 AM)",
-    options: [
-      "Amazon River",
-      "Nile River",
-      "Yangtze River",
-      "Mississippi River",
-    ],
-    answer: "Nile River",
+    options: [],
+
   },
   {
     question: "On weekdays, what time do you usually wake up? (24-hour format, e.g., 07:00 for 7 AM)",
-    options: [
-      "Amazon River",
-      "Nile River",
-      "Yangtze River",
-      "Mississippi River",
-    ],
-    answer: "Nile River",
+    options: [],
   },
   {
     question: "On weekends, what time do you usually wake up? (24-hour format, e.g., 09:30 for 9:30 AM)",
-    options: [
-      "Amazon River",
-      "Nile River",
-      "Yangtze River",
-      "Mississippi River",
-    ],
-    answer: "Nile River",
+    options: [],
   },
 
 ];
