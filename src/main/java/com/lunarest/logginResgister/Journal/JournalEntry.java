36e27765e78672bc96ae1ad6f2e20d2d8a5678ff
Dulsara Abhiman <158539197+DulsaraAbhi@users.noreply.github.com>
2025-03-03 23:58:29 +0530

    implementing the journal

---
+++ src/main/java/com/lunarest/logginResgister/Journal/JournalEntry.java
@@ -0,0 +1,34 @@
+package com.lunarest.logginResgister.journal;
+
+import com.lunarest.logginResgister.appuser.AppUser;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import java.time.LocalDate;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@Entity
+public class JournalEntry {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    private AppUser user;
+
+    private LocalDate date;
+
+    @Column(columnDefinition = "TEXT")
+    private String entryText;
+
+    public JournalEntry(AppUser user, LocalDate date, String entryText) {
+        this.user = user;
+        this.date = date;
+        this.entryText = entryText;
+    }
+}
