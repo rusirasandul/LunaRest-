c1aa93ade949e0dece3aaa38c995745cf229efe4
Dulsara Abhiman <158539197+DulsaraAbhi@users.noreply.github.com>
2025-03-10 23:33:58 +0530

    Creating the music player UI part

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,72 +1,68 @@
-import { Routes, Route } from "react-router-dom";
-import {useState, useEffect } from "react";
+import { Routes, Route, useLocation } from "react-router-dom";
+import { useState, useEffect } from "react";
 import Navbar from "./layouts/Header";
+import Footer from "./layouts/Footer";
 import Home from "./pages/Home";
-import Setting from "./pages/Setting";
 import Journal from "./pages/Journal";
+import Setting from "./pages/Setting";
 import LoginPage from "./pages/LoginPage";
 import SignUpPage from "./pages/SignUpPage";
-import Footer from "./layouts/Footer";
 import AboutAndFAQ from "./pages/About";
 import ContactPage from "./pages/ContactPage";
-import LoadingPage from "./pages/LoadingPage";
 import Dashboard from "./pages/Dashboard.jsx";
 import Recommendation from "./pages/Recommendation.jsx";
 import Article from "./pages/Article.jsx";
 import GoalTracker from "./pages/GoalTracker.jsx";
 import ProfilePage from "./pages/ProfilePage.jsx";
-//import WelcomeCard from "./pages/WelcomeCard";
 import Quiz from "./pages/Quiz";
 import PrivacyPolicy from "./pages/PrivacyPolicy.jsx";
+import LoadingPage from "./pages/LoadingPage";
+import MusicPlayer from "./pages/MusicPlayer"; // Import the player
 
 function App() {
-  const[isLoading,setIsLoading] = useState(true)
+  const [isLoading, setIsLoading] = useState(true);
+  const location = useLocation();
 
-  useEffect (()=>{
-    //loading time or wait for actual resources to load 
-    const timer =setTimeout(()=>{
-      setIsLoading(false)
-    },3000) // this is for 3 s loading time 
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setIsLoading(false);
+    }, 3000);
+    return () => clearTimeout(timer);
+  }, []);
 
-    return () => clearTimeout(timer)
-  },[])
-
-  if (isLoading){
-    return <LoadingPage/>
+  if (isLoading) {
+    return <LoadingPage />;
   }
-  
+
   return (
     <div className="flex flex-col min-h-screen overflow-x-hidden font-heading">
       <Navbar />
-      
+
       <div className="flex-grow">
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/quiz" element={<Quiz/>}/>
+          <Route path="/quiz" element={<Quiz />} />
           <Route path="/journal" element={<Journal />} />
           <Route path="/setting" element={<Setting />} />
           <Route path="/login" element={<LoginPage />} />
           <Route path="/signup" element={<SignUpPage />} />
-          <Route path="goalTracker" element={<GoalTracker/>} />
-          <Route path="/about" element={<AboutAndFAQ/>} />
+          <Route path="/goalTracker" element={<GoalTracker />} />
+          <Route path="/about" element={<AboutAndFAQ />} />
           <Route path="/contact" element={<ContactPage />} />
           <Route path="/dashboard" element={<Dashboard />} />
           <Route path="/recommendation" element={<Recommendation />} />
           <Route path="/article" element={<Article />} />
-          <Route path="/profile" element={<ProfilePage/>} />
-          <Route path="/privacyPolicy" element={<PrivacyPolicy/>} />
+          <Route path="/profile" element={<ProfilePage />} />
+          <Route path="/privacyPolicy" element={<PrivacyPolicy />} />
         </Routes>
       </div>
 
-      <Footer /> 
+      <Footer />
+
+      {/* Music Player - Full player only on Home, Mini on other pages */}
+      {location.pathname !== "/" && <MusicPlayer />}
     </div>
   );
 }
 
 export default App;
-
-/*function App() {
-  const [quizStart, setQuizStart] = useState(false);
-
-  return quizStart ? <Quiz /> : <WelcomeCard setQuizStart={setQuizStart} />;
-} */
