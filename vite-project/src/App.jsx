c8bb76c9e25b3558a5f3abe058b1f2f609eeaf20
Dulsara Abhiman <158539197+DulsaraAbhi@users.noreply.github.com>
2025-03-15 01:51:48 +0530

    Made the mini player

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,23 +1,24 @@
+import React, { useState, useEffect } from 'react';
 import { Routes, Route, useLocation } from "react-router-dom";
-import { useState, useEffect } from "react";
+import { MusicPlayerProvider } from './pages/MusicPlayerContext'; // Fixed import path
 import Navbar from "./layouts/Header";
 import Footer from "./layouts/Footer";
-import Home from "./pages/Home";
 import Journal from "./pages/Journal";
 import Setting from "./pages/Setting";
 import LoginPage from "./pages/LoginPage";
 import SignUpPage from "./pages/SignUpPage";
 import AboutAndFAQ from "./pages/About";
 import ContactPage from "./pages/ContactPage";
-import Dashboard from "./pages/Dashboard.jsx";
-import Recommendation from "./pages/Recommendation.jsx";
-import Article from "./pages/Article.jsx";
-import GoalTracker from "./pages/GoalTracker.jsx";
-import ProfilePage from "./pages/ProfilePage.jsx";
+import Dashboard from "./pages/Dashboard";
+import Recommendation from "./pages/Recommendation";
+import Article from "./pages/Article";
+import GoalTracker from "./pages/GoalTracker";
+import ProfilePage from "./pages/ProfilePage";
 import Quiz from "./pages/Quiz";
-import PrivacyPolicy from "./pages/PrivacyPolicy.jsx";
+import PrivacyPolicy from "./pages/PrivacyPolicy";
 import LoadingPage from "./pages/LoadingPage";
-import MusicPlayer from "./pages/MusicPlayer"; // Import the player
+import MiniPlayer from "./pages/MiniPlayer"; // Fixed import path
+import Home from "./pages/Home"; // Added missing import
 
 function App() {
   const [isLoading, setIsLoading] = useState(true);
@@ -35,33 +36,35 @@ function App() {
   }
 
   return (
-    <div className="flex flex-col min-h-screen overflow-x-hidden font-heading">
-      <Navbar />
-
-      <div className="flex-grow">
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/quiz" element={<Quiz />} />
-          <Route path="/journal" element={<Journal />} />
-          <Route path="/setting" element={<Setting />} />
-          <Route path="/login" element={<LoginPage />} />
-          <Route path="/signup" element={<SignUpPage />} />
-          <Route path="/goalTracker" element={<GoalTracker />} />
-          <Route path="/about" element={<AboutAndFAQ />} />
-          <Route path="/contact" element={<ContactPage />} />
-          <Route path="/dashboard" element={<Dashboard />} />
-          <Route path="/recommendation" element={<Recommendation />} />
-          <Route path="/article" element={<Article />} />
-          <Route path="/profile" element={<ProfilePage />} />
-          <Route path="/privacyPolicy" element={<PrivacyPolicy />} />
-        </Routes>
+    <MusicPlayerProvider>
+      <div className="flex flex-col min-h-screen overflow-x-hidden font-heading">
+        <Navbar />
+        
+        <div className="flex-grow">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/quiz" element={<Quiz />} />
+            <Route path="/journal" element={<Journal />} />
+            <Route path="/setting" element={<Setting />} />
+            <Route path="/login" element={<LoginPage />} />
+            <Route path="/signup" element={<SignUpPage />} />
+            <Route path="/goalTracker" element={<GoalTracker />} />
+            <Route path="/about" element={<AboutAndFAQ />} />
+            <Route path="/contact" element={<ContactPage />} />
+            <Route path="/dashboard" element={<Dashboard />} />
+            <Route path="/recommendation" element={<Recommendation />} />
+            <Route path="/article" element={<Article />} />
+            <Route path="/profile" element={<ProfilePage />} />
+            <Route path="/privacyPolicy" element={<PrivacyPolicy />} />
+          </Routes>
+        </div>
+        
+        <Footer />
+        
+        {/* Show MiniPlayer on all pages except home (/) */}
+        {location.pathname !== "/" && <MiniPlayer />}
       </div>
-
-      <Footer />
-
-      {/* Music Player - Full player only on Home, Mini on other pages */}
-      {location.pathname !== "/" && <MusicPlayer />}
-    </div>
+    </MusicPlayerProvider>
   );
 }
 
