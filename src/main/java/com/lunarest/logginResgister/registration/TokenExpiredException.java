71db4eece842a2673989f6b84ca19a6e77ef20a1
Lakindu <lakinduperera458@gmail.com>
2025-03-16 16:19:55 +0530

    Implement the Invalid EmailException class

--- src/main/java/com/lunarest/logginResgister/registration/TokenExpiredException.java
+++ src/main/java/com/lunarest/logginResgister/registration/TokenExpiredException.java
@@ -1,21 +1,5 @@
 package com.lunarest.logginResgister.registration;
 
-public class InvalidEmailException extends RuntimeException {
-    public InvalidEmailException(String message) {
-        super(message);
-    }
-}
-
-package com.lunarest.logginResgister.registration;
-
-public class TokenNotFoundException extends RuntimeException {
-    public TokenNotFoundException(String message) {
-        super(message);
-    }
-}
-
-package com.lunarest.logginResgister.registration;
-
 public class EmailAlreadyConfirmedException extends RuntimeException {
     public EmailAlreadyConfirmedException(String message) {
         super(message);
@@ -26,6 +10,13 @@ package com.lunarest.logginResgister.registration;
 
 public class TokenExpiredException extends RuntimeException {
     public TokenExpiredException(String message) {
+        super(message);
+    }
+}
+package com.lunarest.logginResgister.registration;
+
+public class TokenNotFoundException extends RuntimeException {
+    public TokenNotFoundException(String message) {
         super(message);
     }
 }
