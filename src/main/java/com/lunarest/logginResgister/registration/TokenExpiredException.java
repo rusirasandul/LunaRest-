5734d9be50cf09d7e96b979492635aa183dc8298
Lakindu <lakinduperera458@gmail.com>
2025-03-16 16:21:17 +0530

    implement the TokenExpiredException class

--- src/main/java/com/lunarest/logginResgister/registration/TokenExpiredException.java
+++ src/main/java/com/lunarest/logginResgister/registration/TokenExpiredException.java
@@ -1,22 +1,7 @@
 package com.lunarest.logginResgister.registration;
 
-public class EmailAlreadyConfirmedException extends RuntimeException {
-    public EmailAlreadyConfirmedException(String message) {
-        super(message);
-    }
-}
-
-package com.lunarest.logginResgister.registration;
-
 public class TokenExpiredException extends RuntimeException {
     public TokenExpiredException(String message) {
-        super(message);
-    }
-}
-package com.lunarest.logginResgister.registration;
-
-public class TokenNotFoundException extends RuntimeException {
-    public TokenNotFoundException(String message) {
         super(message);
     }
 }
---
+++ src/main/java/com/lunarest/logginResgister/registration/TokenNotFoundException.java
@@ -0,0 +1,16 @@
+package com.lunarest.logginResgister.registration;
+
+public class EmailAlreadyConfirmedException extends RuntimeException {
+    public EmailAlreadyConfirmedException(String message) {
+        super(message);
+    }
+}
+
+
+package com.lunarest.logginResgister.registration;
+
+public class TokenNotFoundException extends RuntimeException {
+    public TokenNotFoundException(String message) {
+        super(message);
+    }
+}
